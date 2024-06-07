f653ec692ada686563c8bf94c22d9bae4b22cdf2
Whitney Navalia <136854331+Whitney2695@users.noreply.github.com>
2024-06-07 09:09:39 -0700

    notebook endpoints with database

---
+++ src/config/sql.config.ts
@@ -0,0 +1,22 @@
+import * as mysql from 'mysql';
+import * as dotenv from 'dotenv';
+
+// Load environment variables from .env file
+dotenv.config();
+
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error('Error connecting to MySQL database:', err);
+    return;
+  }
+  console.log('Connected to MySQL database');
+});
+
+export default connection;
